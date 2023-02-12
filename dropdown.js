e087c6c7b8fbe0fdb1b3d4a5e7d5ec7bba56f544
Odiase-Omoighe Samuel <odiasesamuel4@gmail.com>
2023-02-12 23:59:20 +0100

    add scroll to top feature

--- dropdown.js
+++ dropdown.js
@@ -2,15 +2,29 @@
 const open_btn = document.querySelector(".open-btn");
 const close_btn = document.querySelector(".close-btn");
 const nav = document.querySelectorAll(".nav");
+const btnScrollToTop = document.getElementById('scroll-to-top');
 
 open_btn.addEventListener("click", () => {
   nav.forEach((nav_el) => nav_el.classList.add("visible"));
   open_btn.style.opacity = "0";
   document.body.style.overflow = 'hidden';
+  btnScrollToTop.style.opacity = '0'
 });
 
 close_btn.addEventListener("click", () => {
   nav.forEach((nav_el) => nav_el.classList.remove("visible"));
   open_btn.style.opacity = "1";
   document.body.style.overflow = 'visible';
+  btnScrollToTop.style.opacity = '1'
 });
+
+
+
+// Scroll to top
+btnScrollToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+})
